ed2253a064229275bca5d716ad50db75d995d443
ShraddhaTest <shraddhasoni1609@gmail.com>
2018-01-26 10:34:44 +0530

    :QA

---
+++ BDDTest/src/main/java/BDDTest/BDDTest/App.java
@@ -0,0 +1,14 @@
+package BDDTest.BDDTest;
+
+/**
+ * Hello world!
+ * Hello to new QA
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
